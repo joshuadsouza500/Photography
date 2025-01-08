c11981942ccf6bbd955d8e546f7218e165c0d685
joshua <joshuadsouza500@gmail.com>
2025-01-09 00:18:42 +0300

    Changedd header text2

--- app/components/ui/Button.jsx
+++ app/components/ui/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button({ text, className }) {
   return (
-    <button className="bg-[#0c0c0c]  border hover:border-[#3e3e3e] hover:scale-[1.01] rounded-3xl text-white px-4 md:px-5 py-3   border-slate-100 hover:bg-slate-50 hover:text-black md:h-12 w-36 ">
+    <button className="bg-[#0c0c0c]  border hover:border-[#3e3e3e] hover:scale-[1.01] rounded-3xl text-white px-3 md:px-4 md:py-3 py-2   border-slate-100 hover:bg-slate-50 hover:text-black md:h-12 w-36 ">
       {text}
     </button>
   );
